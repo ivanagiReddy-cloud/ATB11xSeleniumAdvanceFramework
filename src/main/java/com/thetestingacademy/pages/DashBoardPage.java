06ccad24f6ccc13b43c89afb61aba65d3bb156e0
Nagireddy <mulisivanagireddy@gmail.com>
2025-06-09 17:39:26 +0530

    Initial commit

---
+++ src/main/java/com/thetestingacademy/pages/DashBoardPage.java
@@ -0,0 +1,4 @@
+package com.thetestingacademy.pages;
+
+public class DashBoard {
+}
---
+++ src/test/java/com/thetestingacademy/tests/dryRun/DryRun.java
@@ -0,0 +1,15 @@
+package com.thetestingacademy.dryRun;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class DryRun {
+
+    @Test
+    public void test_DryRun(){
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://awesomeqa.com/hr/web/index.php/auth/login");
+    }
+}
